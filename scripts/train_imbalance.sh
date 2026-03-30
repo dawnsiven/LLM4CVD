221a901744899e9ca449cfba060b226ea5ca3be4
Jiarong Zhu <zjr2018000@163.com>
2026-03-30 22:19:12 +0800

    调整imbalanced的相关代码

--- scripts/train_imbalance.sh
+++ scripts/train_imbalance.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DATASET_NAME=$1
 MODEL_NAME=$2
+LENGTH="0-512"
 POS_RATIO=$3
 CUDA=${4:-"0"}
 
@@ -15,64 +16,64 @@ if [[ ${BASH_VERSINFO[0]} -lt 4 ]]; then
     exit 1
 fi
 
-mkdir -p "outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/"
+mkdir -p "outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${LENGTH}_${POS_RATIO}/"
 
 echo "POS_RATIO: $(echo $POS_RATIO)"
 
 if [[ "$MODEL_NAME" == "GraphCodeBERT" ]]; then
 CUDA_VISIBLE_DEVICES="${CUDA}" python ${MODEL_NAME}/run.py \
-    --output_dir="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/" \
-    --csv_path="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/results.csv" \
+    --output_dir="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${LENGTH}_${POS_RATIO}/" \
+    --csv_path="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${LENGTH}_${POS_RATIO}/results.csv" \
     --tokenizer_name=microsoft/codebert-base \
     --model_name_or_path=microsoft/codebert-base \
     --do_train \
     --do_eval \
     --do_test \
-    --train_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_train.json" \
-    --eval_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_validate.json" \
-    --test_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_test.json" \
+    --train_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_train.json" \
+    --eval_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_validate.json" \
+    --test_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_test.json" \
     --epoch 5 \
     --block_size 512 \
     --train_batch_size 32 \
     --eval_batch_size 64 \
     --evaluate_during_training \
     --seed 42 \
-    2>"outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/train_${MODEL_NAME}_${DATASET_NAME}_${POS_RATIO}.log"
+    2>"outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/train_${MODEL_NAME}_${DATASET_NAME}_${LENGTH}_${POS_RATIO}.log"
 elif [[ "$MODEL_NAME" == "UniXcoder" ]]; then
 CUDA_VISIBLE_DEVICES="${CUDA}" python ${MODEL_NAME}/run.py \
-    --output_dir="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/" \
-    --csv_path="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/results.csv" \
+    --output_dir="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${LENGTH}_${POS_RATIO}/" \
+    --csv_path="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${LENGTH}_${POS_RATIO}/results.csv" \
     --model_name_or_path=microsoft/codebert-base \
     --do_train \
     --do_eval \
     --do_test \
-    --train_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_train.json" \
-    --eval_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_validate.json" \
-    --test_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_test.json" \
+    --train_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_train.json" \
+    --eval_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_validate.json" \
+    --test_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_test.json" \
     --num_train_epochs 5 \
     --block_size 512 \
     --train_batch_size 32 \
     --eval_batch_size 64 \
     --seed 42 \
-    2>"outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/train_${MODEL_NAME}_${DATASET_NAME}_${POS_RATIO}.log"
+    2>"outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/train_${MODEL_NAME}_${DATASET_NAME}_${LENGTH}_${POS_RATIO}.log"
 else
 CUDA_VISIBLE_DEVICES="${CUDA}" python ${MODEL_NAME}/run.py \
-    --output_dir="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/" \
-    --csv_path="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/results.csv" \
+    --output_dir="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${LENGTH}_${POS_RATIO}/" \
+    --csv_path="outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${LENGTH}_${POS_RATIO}/results.csv" \
     --model_type=roberta \
     --tokenizer_name=microsoft/codebert-base \
     --model_name_or_path=microsoft/codebert-base \
     --do_train \
     --do_eval \
     --do_test \
-    --train_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_train.json" \
-    --eval_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_validate.json" \
-    --test_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${POS_RATIO}_test.json" \
+    --train_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_train.json" \
+    --eval_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_validate.json" \
+    --test_data_file="data/${DATASET_NAME}_subsampled/alpaca/${DATASET_NAME}_${LENGTH}_${POS_RATIO}_test.json" \
     --epoch 5 \
     --block_size 512 \
     --train_batch_size 32 \
     --eval_batch_size 64 \
     --evaluate_during_training \
     --seed 42 \
-    2>"outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/train_${MODEL_NAME}_${DATASET_NAME}_${POS_RATIO}.log"
+    2>"outputs/${MODEL_NAME}_imbalance/${DATASET_NAME}_${POS_RATIO}/train_${MODEL_NAME}_${DATASET_NAME}_${LENGTH}_${POS_RATIO}.log"
 fi
